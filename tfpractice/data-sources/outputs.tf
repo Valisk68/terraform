a6fbbb273508b85363ee0a53c792d0ee2e6bc48e
“valisk68” <shaikvalims@gmail.com>
2026-03-08 16:49:30 +0530

    data source public ip

--- tfpractice/data-sources/outputs.tf
+++ tfpractice/data-sources/outputs.tf
@@ -4,4 +4,10 @@ output "ami_id" {
 
 output "instance_info" {
     value = data.aws_instance.terraform_test.private_ip
+   
+}
+
+output "instance_public_ip" {
+    value = data.aws_instance.terraform_test.public_ip
+   
 }
